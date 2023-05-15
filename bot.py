fbb608610a9e9f01d4f8f0a17423ea8e145b1d95
mazin112 <120086917+mazin112@users.noreply.github.com>
2023-05-16 01:22:34 +0200

    bot.py

---
+++ bot.py
@@ -0,0 +1,76 @@
+import os
+
+import requests
+
+from pyrogram import Client, filters
+
+# Create a Telegram bot
+
+bot = Client("mega_bot", api_id="YOUR_API_ID", api_hash="YOUR_API_HASH")
+
+# Define a command to upload files to Mega
+
+@bot.on(filters.command("upload"))
+
+async def upload(message):
+
+    # Get the file path from the message
+
+    file_path = message.text
+
+    # Upload the file to Mega
+
+    response = requests.post("https://mega.nz/api/upload", files={"file": open(file_path, "rb")})
+
+    # Check the response status code
+
+    if response.status_code == 200:
+
+        # The file was uploaded successfully
+
+        await message.reply("File uploaded successfully!")
+
+    else:
+
+        # The file failed to upload
+
+        await message.reply("File upload failed!")
+
+# Deploy the bot to Heroku
+
+heroku_app = "YOUR_HEROKU_APP_NAME"
+
+heroku_region = "YOUR_HEROKU_REGION"
+
+# Create a Procfile
+
+Procfile = """
+
+web: python bot.py
+
+"""
+
+# Create a requirements.txt file
+
+requirements.txt = """
+
+requests
+
+pyrogram
+
+"""
+
+# Deploy the bot to Heroku
+
+heroku login
+
+heroku create --region $heroku_region $heroku_app
+
+heroku addons:add heroku-postgresql:hobby-dev
+
+heroku git:remote -a $heroku_app
+
+heroku push
+
+heroku run python bot.py
+
